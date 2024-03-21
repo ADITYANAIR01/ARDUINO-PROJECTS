9b521012962ca3f36cdfbeb5fdcc9f8b39319b62
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2024-03-21 21:39:02 +0530

    PIR SENSOR code

--- PIR SENSOR/pir_sensor.ino
+++ PIR SENSOR/pir_sensor.ino
@@ -5,7 +5,7 @@ int calibrationTime = 30;
 long unsigned int lowIn;         
 
 //the amount of milliseconds the sensor has to be low 
-long unsigned int pause = 5000;  
+long unsigned int pause = 20;  
 
 boolean lockLow = true;
 boolean takeLowTime;  
@@ -23,11 +23,11 @@ void setup(){
   Serial.print("calibrating sensor ");
     for(int i = 0; i < calibrationTime; i++){
       Serial.print(".");
-      delay(1000);
+      delay(20);
       }
     Serial.println(" done");
     Serial.println("SENSOR ACTIVE");
-    delay(50);
+    delay(10);
   }
 
 void loop(){
@@ -38,9 +38,9 @@ void loop(){
          lockLow = false;            
          Serial.println("---");
          Serial.print("motion detected at ");
-         Serial.print(millis()/1000);
+         Serial.print(millis()/20);
          Serial.println(" sec"); 
-         delay(50);
+         delay(10);
          }         
          takeLowTime = true;
        }
@@ -57,9 +57,9 @@ void loop(){
            //a new motion sequence has been detected
            lockLow = true;                        
            Serial.print("motion ended at ");      
-           Serial.print((millis() - pause)/1000);
+           Serial.print((millis() - pause)/20);
            Serial.println(" sec");
-           delay(50);
+           delay(10);
            }
        }
   }
