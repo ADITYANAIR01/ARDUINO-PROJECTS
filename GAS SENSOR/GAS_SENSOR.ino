7f8ae1309657745608bcd6501563361b33b579a3
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2024-03-21 20:20:20 +0530

    GAS SENSOR CODE

--- GAS SENSOR/GAS_SENSOR.ino
+++ GAS SENSOR/GAS_SENSOR.ino
@@ -1,15 +1,21 @@
-int mq=A0; // PINS TO BE CONNECT
-int x;
+// Define the analog pin connected to the MQ sensor
+const int MQ_PIN = A0;
 
-void setup() 
-{
-  pinMode(A0,INPUT);
+void setup() {
+  // Set the pin mode to INPUT
+  pinMode(MQ_PIN, INPUT);
+  
+  // Initialize serial communication
   Serial.begin(9600);
 }
 
-void loop() 
-{
-  x=analogRead(A0);
-  Serial.println(x);
+void loop() {
+  // Read the analog value from the MQ sensor
+  int mqSensorValue = analogRead(MQ_PIN);
+  
+  // Print the sensor value to the serial monitor
+  Serial.println(mqSensorValue);
+  
+  // Delay for 500 milliseconds
   delay(500);
 }
