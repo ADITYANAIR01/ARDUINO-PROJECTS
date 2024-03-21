f074842244e4c20b9c66c98e8624f150eb089086
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2024-03-21 20:09:11 +0530

    DH11 TEMPERATURE CODE

--- DH11 TEMPERATURE/DHT.ino
+++ DH11 TEMPERATURE/DHT.ino
@@ -1,46 +1,27 @@
-#include "DHT.h"
-
+#include <DHT.h>
 #define DHTPIN 2     // what pin we're connected to
 #define DHTTYPE DHT11   // DHT 11
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("DHTxx test!");
+  Serial.println("DHT temp test!");
 
   dht.begin();
 }
 
 void loop() {
-  delay(2000);
-
-  float h = dht.readHumidity();
+  delay(1000);
 
   float t = dht.readTemperature();
 
-  float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
+  // Check if the read failed and exit early (to try again).
+  if (isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
     return;
   }
 
-  float hif = dht.computeHeatIndex(f, h);
-
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
   Serial.print("Temperature: ");
   Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
+  Serial.println(" *C");
 }
