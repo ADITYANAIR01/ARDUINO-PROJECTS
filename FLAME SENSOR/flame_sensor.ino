6ba796927961d69c09713d614d497b9bf13fe532
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2024-04-04 22:11:46 +0530

    flame_sensor

---
+++ FLAME SENSOR/flame_sensor.ino
@@ -0,0 +1,27 @@
+const int ledPin = 9;  // Changed from buzzerPin to ledPin
+const int fireSensorPin = 8;
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  // Changed from buzzerPin to ledPin
+  pinMode(fireSensorPin, INPUT);
+}
+
+void loop()
+{
+  int fireValue = digitalRead(fireSensorPin);
+  Serial.println(fireValue);
+  
+  if(fireValue == 0)
+  {
+    digitalWrite(ledPin, HIGH);  // Changed from buzzerPin to ledPin
+    delay(200);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);  // Changed from buzzerPin to ledPin
+  }
+  
+  delay(500);
+}
