5eb9634a6c98b5b01964a5e6a220d44a8c590741
Aditya Nair <121816281+ADITYANAIR01@users.noreply.github.com>
2025-03-21 18:24:29 +0530

    sos button with schematic diagram

--- INDIVIDUAL SENSOR CODES/SOS_BUTTON/SOS_BUTTON.ino
+++ INDIVIDUAL SENSOR CODES/SOS_BUTTON/SOS_BUTTON.ino
@@ -1,23 +1,19 @@
 # define BUTTON_PIN 2
-# define LED_PIN 3
 
 void setup() {
-  pinMode(2, INPUT);
-  pinMode(3, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  bool value = digitalRead(2);
-  if (value == 1) {
-    digitalWrite(LED_PIN, LOW);
-   //  Serial.println(" THERE IS NO EMERGENCY ");
-    
-  } else {
-    digitalWrite(LED_PIN,HIGH);
-    Serial.println(" SOS BUTTON PUSHED ");
-    Serial.println("CHANGING TO HOUSE  & MONITORING ");
+ int button;
+ button = digitalRead(BUTTON_PIN);
 
-    delay(10000);
-  }
+ if(button == LOW){
+  Serial.println("ON");
+ } else {
+  Serial.println("OFF");
+
+ }
+ delay(1000);
 }
